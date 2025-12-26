aef438a0281dacee46af2acc4d7e7d881174831d
jhzhou002 <318352733@qq.com>
2025-12-26 22:00:13 +0800

    feat: 更新数据库配置为本地环境

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const db = mysql.createPool({
-    host: '49.235.74.98',
-    user: 'remote',
-    password: 'Zhjh0704.',
-    database: 'opendigger',
+    host: localhost,
+    user: username,
+    password: pwd,
+    database: db,
     connectionLimit: 10,
     waitForConnections: true,
     queueLimit: 0,
